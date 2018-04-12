c11bb150c5f74125156baa1c4e0a7be5df406cfd
Natalia Portillo <claunia@claunia.com>
2018-04-12 10:20:31 +0100

    Add file headers.

--- Cicm.Database/Schemas/Sql/V2.cs
+++ Cicm.Database/Schemas/Sql/V2.cs
@@ -1,4 +1,33 @@
-﻿namespace Cicm.Database.Schemas.Sql
+﻿/******************************************************************************
+// Canary Islands Computer Museum Website
+// ----------------------------------------------------------------------------
+//
+// Filename       : V2.cs
+// Author(s)      : Natalia Portillo <claunia@claunia.com>
+//
+// --[ Description ] ----------------------------------------------------------
+//
+//     Contains SQL queries to create the database version 2.
+//
+// --[ License ] --------------------------------------------------------------
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the
+//     License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// ----------------------------------------------------------------------------
+// Copyright © 2003-2018 Natalia Portillo
+*******************************************************************************/
+namespace Cicm.Database.Schemas.Sql
 {
     public static class V2
     {
