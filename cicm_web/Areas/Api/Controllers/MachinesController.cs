16635951d87a0ee5e8e4eca3d98f0ba00d627e81
Natalia Portillo <claunia@claunia.com>
2018-08-11 10:50:31 +0100

    Add API controllers.

---
+++ cicm_web/Areas/Api/Controllers/MachinesController.cs
@@ -0,0 +1,69 @@
+﻿/******************************************************************************
+// Canary Islands Computer Museum Website
+// ----------------------------------------------------------------------------
+//
+// Filename       : MachinesController.cs
+// Author(s)      : Natalia Portillo <claunia@claunia.com>
+//
+// --[ Description ] ----------------------------------------------------------
+//
+//     Machines api controller
+//
+// --[ License ] --------------------------------------------------------------
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the
+//     License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// ----------------------------------------------------------------------------
+// Copyright © 2003-2018 Natalia Portillo
+*******************************************************************************/
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cicm.Database.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cicm_web.Areas.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MachinesController : ControllerBase
+    {
+        readonly cicmContext _context;
+
+        public MachinesController(cicmContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Machines
+        [HttpGet]
+        public IEnumerable<Machine> GetMachines()
+        {
+            return _context.Machines;
+        }
+
+        // GET: api/Machines/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMachine([FromRoute] int id)
+        {
+            if(!ModelState.IsValid) return BadRequest(ModelState);
+
+            Machine machine = await _context.Machines.FindAsync(id);
+
+            if(machine == null) return NotFound();
+
+            return Ok(machine);
+        }
+    }
+}
