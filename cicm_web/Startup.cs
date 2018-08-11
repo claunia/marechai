53f7315b2ed367beaa5e8c81e7847045626412a3
Natalia Portillo <claunia@claunia.com>
2018-08-11 11:15:31 +0100

    Correct areas routing.

--- cicm_web/Startup.cs
+++ cicm_web/Startup.cs
@@ -66,10 +66,10 @@ namespace cicm_web
 
             app.UseStaticFiles();
 
-            app.UseMvc(routes => { routes.MapRoute("default", "{controller=Home}/{action=Index}/{id?}").MapRoute(
+            app.UseMvc(routes => { routes.MapRoute(
                 name: "areas",
                 template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-            ); });
+            ).MapRoute("default", "{controller=Home}/{action=Index}/{id?}"); });
         }
     }
 }
