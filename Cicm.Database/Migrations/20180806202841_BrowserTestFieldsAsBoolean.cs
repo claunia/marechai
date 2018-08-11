4dbb43cedd2eeb7474261b14085718207256cc66
Natalia Portillo <claunia@claunia.com>
2018-08-11 11:25:02 +0100

    Add missing migration.

---
+++ Cicm.Database/Migrations/20180806202841_BrowserTestFieldsAsBoolean.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cicm.Database.Migrations
+{
+    public partial class BrowserTestFieldsAsBoolean : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
