0f29a8dded8374246debaf2b174cfb201516dd27
Natalia Portillo <claunia@claunia.com>
2019-05-18 13:31:33 +0100

    Fix MySQL bool interpretation.

--- cicm_web/Startup.cs
+++ cicm_web/Startup.cs
@@ -60,7 +60,7 @@ namespace cicm_web
             #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
             services.AddDbContext<cicmContext>(options => options
                                                          .UseLazyLoadingProxies()
-                                                         .UseMySql("server=localhost;port=3306;user=cicm;password=cicmpass;database=cicm"));
+                                                         .UseMySql("server=localhost;port=3306;user=cicm;password=cicmpass;database=cicm;TreatTinyAsBoolean=false"));
 
             services.AddMvc();
         }
