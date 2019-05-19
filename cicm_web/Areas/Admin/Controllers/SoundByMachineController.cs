b7df0998aa00e0aad0b4578c490f1797eae881c1
Natalia Portillo <claunia@claunia.com>
2019-05-20 00:38:13 +0100

    Optimize view of sound synthetizers by machine admin page.

--- cicm_web/Areas/Admin/Controllers/SoundByMachineController.cs
+++ cicm_web/Areas/Admin/Controllers/SoundByMachineController.cs
@@ -1,10 +1,12 @@
 using System.Linq;
 using System.Threading.Tasks;
 using Cicm.Database.Models;
+using cicm_web.Areas.Admin.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 
 namespace cicm_web.Areas.Admin.Controllers
 {
@@ -12,7 +14,7 @@ namespace cicm_web.Areas.Admin.Controllers
     [Authorize]
     public class SoundByMachineController : Controller
     {
-        private readonly cicmContext _context;
+        readonly cicmContext _context;
 
         public SoundByMachineController(cicmContext context)
         {
@@ -22,26 +24,26 @@ namespace cicm_web.Areas.Admin.Controllers
         // GET: SoundByMachine
         public async Task<IActionResult> Index()
         {
-            var cicmContext = _context.SoundByMachine.Include(s => s.Machine).Include(s => s.SoundSynth);
-            return View(await cicmContext.OrderBy(s => s.Machine.Name).ThenBy(s => s.SoundSynth.Name).ToListAsync());
+            IIncludableQueryable<SoundByMachine, SoundSynth> cicmContext =
+                _context.SoundByMachine.Include(s => s.Machine).Include(s => s.SoundSynth);
+            return View(await cicmContext.OrderBy(s => s.Machine.Name).ThenBy(s => s.SoundSynth.Name)
+                                         .Select(s => new SoundByMachineViewModel
+                                          {
+                                              Id         = s.Id,
+                                              Machine    = s.Machine.Name,
+                                              SoundSynth = s.SoundSynth.Name
+                                          }).ToListAsync());
         }
 
         // GET: SoundByMachine/Details/5
         public async Task<IActionResult> Details(long? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if(id == null) return NotFound();
 
-            var soundByMachine = await _context.SoundByMachine
-                .Include(s => s.Machine)
-                .Include(s => s.SoundSynth)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (soundByMachine == null)
-            {
-                return NotFound();
-            }
+            SoundByMachine soundByMachine = await _context.SoundByMachine
+                                                          .Include(s => s.Machine).Include(s => s.SoundSynth)
+                                                          .FirstOrDefaultAsync(m => m.Id == id);
+            if(soundByMachine == null) return NotFound();
 
             return View(soundByMachine);
         }
@@ -49,7 +51,7 @@ namespace cicm_web.Areas.Admin.Controllers
         // GET: SoundByMachine/Create
         public IActionResult Create()
         {
-            ViewData["MachineId"] = new SelectList(_context.Machines, "Id", "Name");
+            ViewData["MachineId"]    = new SelectList(_context.Machines,    "Id", "Name");
             ViewData["SoundSynthId"] = new SelectList(_context.SoundSynths, "Id", "Name");
             return View();
         }
@@ -61,13 +63,14 @@ namespace cicm_web.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("SoundSynthId,MachineId,Id")] SoundByMachine soundByMachine)
         {
-            if (ModelState.IsValid)
+            if(ModelState.IsValid)
             {
                 _context.Add(soundByMachine);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["MachineId"] = new SelectList(_context.Machines, "Id", "Name", soundByMachine.MachineId);
+
+            ViewData["MachineId"]    = new SelectList(_context.Machines,    "Id", "Name", soundByMachine.MachineId);
             ViewData["SoundSynthId"] = new SelectList(_context.SoundSynths, "Id", "Name", soundByMachine.SoundSynthId);
             return View(soundByMachine);
         }
@@ -75,17 +78,12 @@ namespace cicm_web.Areas.Admin.Controllers
         // GET: SoundByMachine/Edit/5
         public async Task<IActionResult> Edit(long? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if(id == null) return NotFound();
 
-            var soundByMachine = await _context.SoundByMachine.FindAsync(id);
-            if (soundByMachine == null)
-            {
-                return NotFound();
-            }
-            ViewData["MachineId"] = new SelectList(_context.Machines, "Id", "Name", soundByMachine.MachineId);
+            SoundByMachine soundByMachine = await _context.SoundByMachine.FindAsync(id);
+            if(soundByMachine == null) return NotFound();
+
+            ViewData["MachineId"]    = new SelectList(_context.Machines,    "Id", "Name", soundByMachine.MachineId);
             ViewData["SoundSynthId"] = new SelectList(_context.SoundSynths, "Id", "Name", soundByMachine.SoundSynthId);
             return View(soundByMachine);
         }
@@ -95,34 +93,29 @@ namespace cicm_web.Areas.Admin.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(long id, [Bind("SoundSynthId,MachineId,Id")] SoundByMachine soundByMachine)
+        public async Task<IActionResult> Edit(
+            long id, [Bind("SoundSynthId,MachineId,Id")] SoundByMachine soundByMachine)
         {
-            if (id != soundByMachine.Id)
-            {
-                return NotFound();
-            }
+            if(id != soundByMachine.Id) return NotFound();
 
-            if (ModelState.IsValid)
+            if(ModelState.IsValid)
             {
                 try
                 {
                     _context.Update(soundByMachine);
                     await _context.SaveChangesAsync();
                 }
-                catch (DbUpdateConcurrencyException)
+                catch(DbUpdateConcurrencyException)
                 {
-                    if (!SoundByMachineExists(soundByMachine.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    if(!SoundByMachineExists(soundByMachine.Id)) return NotFound();
+
+                    throw;
                 }
+
                 return RedirectToAction(nameof(Index));
             }
-            ViewData["MachineId"] = new SelectList(_context.Machines, "Id", "Name", soundByMachine.MachineId);
+
+            ViewData["MachineId"]    = new SelectList(_context.Machines,    "Id", "Name", soundByMachine.MachineId);
             ViewData["SoundSynthId"] = new SelectList(_context.SoundSynths, "Id", "Name", soundByMachine.SoundSynthId);
             return View(soundByMachine);
         }
@@ -130,35 +123,29 @@ namespace cicm_web.Areas.Admin.Controllers
         // GET: SoundByMachine/Delete/5
         public async Task<IActionResult> Delete(long? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if(id == null) return NotFound();
 
-            var soundByMachine = await _context.SoundByMachine
-                .Include(s => s.Machine)
-                .Include(s => s.SoundSynth)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (soundByMachine == null)
-            {
-                return NotFound();
-            }
+            SoundByMachine soundByMachine = await _context.SoundByMachine
+                                                          .Include(s => s.Machine).Include(s => s.SoundSynth)
+                                                          .FirstOrDefaultAsync(m => m.Id == id);
+            if(soundByMachine == null) return NotFound();
 
             return View(soundByMachine);
         }
 
         // POST: SoundByMachine/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost]
+        [ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(long id)
         {
-            var soundByMachine = await _context.SoundByMachine.FindAsync(id);
+            SoundByMachine soundByMachine = await _context.SoundByMachine.FindAsync(id);
             _context.SoundByMachine.Remove(soundByMachine);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool SoundByMachineExists(long id)
+        bool SoundByMachineExists(long id)
         {
             return _context.SoundByMachine.Any(e => e.Id == id);
         }
