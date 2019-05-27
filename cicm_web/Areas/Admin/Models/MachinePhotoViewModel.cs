662e5de16407aaebb01e7fabb95a6c09827e35a0
Natalia Portillo <claunia@claunia.com>
2019-05-27 15:50:26 +0100

    Use properties in machine photo viewmodel so annotations are used.

--- cicm_web/Areas/Admin/Models/MachinePhotoViewModel.cs
+++ cicm_web/Areas/Admin/Models/MachinePhotoViewModel.cs
@@ -5,12 +5,12 @@ namespace cicm_web.Areas.Admin.Models
 {
     public class MachinePhotoViewModel : BaseViewModel<Guid>
     {
-        public string Author;
-        public string License;
-        public string Machine;
+        public string Author  { get; set; }
+        public string License { get; set; }
+        public string Machine { get; set; }
         [DisplayName("Uploaded")]
-        public DateTime UploadDate;
+        public DateTime UploadDate { get; set; }
         [DisplayName("Uploaded by")]
-        public string UploadUser;
+        public string UploadUser { get; set; }
     }
 }
