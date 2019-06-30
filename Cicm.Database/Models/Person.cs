04987cfe12f92556e775ac891e47a195ecefee63
Natalia Portillo <claunia@claunia.com>
2019-06-30 20:41:38 +0100

    Add display attributes to person model.

--- Cicm.Database/Models/Person.cs
+++ Cicm.Database/Models/Person.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -10,15 +11,21 @@ namespace Cicm.Database.Models
         [Required]
         public string Name { get; set; }
         [Required]
-        public string Surname { get;                           set; }
-        public virtual Iso31661Numeric CountryOfBirth   { get; set; }
-        public         DateTime        BirthDate        { get; set; }
-        public         DateTime?       DeathDate        { get; set; }
-        public         string          Webpage          { get; set; }
-        public         string          Twitter          { get; set; }
-        public         string          Facebook         { get; set; }
-        public         Guid            Photo            { get; set; }
-        public         int?            DocumentPersonId { get; set; }
+        public string Surname { get; set; }
+        [DisplayName("Country of birth")]
+        public virtual Iso31661Numeric CountryOfBirth { get; set; }
+        [DisplayName("Birth date")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+        [DisplayName("Date of death")]
+        [DataType(DataType.Date)]
+        public DateTime? DeathDate { get; set; }
+        [Url]
+        public string Webpage { get;          set; }
+        public string Twitter          { get; set; }
+        public string Facebook         { get; set; }
+        public Guid   Photo            { get; set; }
+        public int?   DocumentPersonId { get; set; }
 
         [NotMapped]
         public string FullName => $"{Name} {Surname}";
