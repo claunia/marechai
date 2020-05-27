3e0bd57bebe578f631a1234c50b16c1c45a4d987
Natalia Portillo <claunia@claunia.com>
2020-05-27 18:46:01 +0100

    Add instruction set editing in admin view.

--- Marechai/Pages/Admin/Details/InstructionSet.razor.cs
+++ Marechai/Pages/Admin/Details/InstructionSet.razor.cs
@@ -1,12 +1,15 @@
+using System;
 using System.Threading.Tasks;
+using Blazorise;
+using Marechai.Shared;
 using Microsoft.AspNetCore.Components;
 
 namespace Marechai.Pages.Admin.Details
 {
     public partial class InstructionSet
     {
-        bool                           _editable;
-        bool                           _loaded;
+        bool                                    _editing;
+        bool                                    _loaded;
         Database.Models.InstructionSet _model;
         [Parameter]
         public int Id { get; set; }
@@ -23,7 +26,51 @@ namespace Marechai.Pages.Admin.Details
 
             _model = await Service.GetAsync(Id);
 
+            _editing = NavigationManager.ToBaseRelativePath(NavigationManager.Uri).ToLowerInvariant().
+                                         StartsWith("admin/instruction_sets/edit/",
+                                                    StringComparison.InvariantCulture);
+
             StateHasChanged();
+        }
+
+        void OnEditClicked()
+        {
+            _editing = true;
+            StateHasChanged();
+        }
+
+        async void OnCancelClicked()
+        {
+            _editing = false;
+            _model   = await Service.GetAsync(Id);
+            StateHasChanged();
+        }
+
+        async void OnSaveClicked()
+        {
+            if(string.IsNullOrWhiteSpace(_model.Name) ||
+               _model.Name.Length > 45                ||
+               !Service.VerifyUnique(_model.Name))
+                return;
+
+            _editing = false;
+            await Service.UpdateAsync(_model);
+            _model = await Service.GetAsync(Id);
+            StateHasChanged();
+        }
+
+        void ValidateName(ValidatorEventArgs e)
+        {
+            Validators.ValidateString(e, L["Instruction set name cannot contain more than 45 characters."], 45);
+
+            if(e.Status != ValidationStatus.Success)
+                return;
+
+            if(Service.VerifyUnique(_model.Name))
+                return;
+
+            e.Status    = ValidationStatus.Error;
+            e.ErrorText = L["Instruction set name must be unique."];
         }
     }
 }
--- Marechai/Services/InstructionSetsService.cs
+++ Marechai/Services/InstructionSetsService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,9 +14,28 @@ namespace Marechai.Services
         public InstructionSetsService(MarechaiContext context) => _context = context;
 
         public async Task<List<InstructionSet>> GetAsync() =>
-            await _context.InstructionSets.OrderBy(s => s.Name).ToListAsync();
+            await _context.InstructionSets.OrderBy(e => e.Name).Select(e => new InstructionSet
+            {
+                Name = e.Name, Id = e.Id
+            }).ToListAsync();
 
-        public async Task<InstructionSet> GetAsync(int id) => await _context.InstructionSets.FindAsync(id);
+        public async Task<InstructionSet> GetAsync(int id) =>
+            await _context.InstructionSets.Where(e => e.Id == id).Select(e => new InstructionSet
+            {
+                Name = e.Name, Id = e.Id
+            }).FirstOrDefaultAsync();
+
+        public async Task UpdateAsync(InstructionSet viewModel)
+        {
+            InstructionSet model = await _context.InstructionSets.FindAsync(viewModel.Id);
+
+            if(model is null)
+                return;
+
+            model.Name = viewModel.Name;
+
+            await _context.SaveChangesAsync();
+        }
 
         public async Task DeleteAsync(int id)
         {
@@ -28,5 +48,9 @@ namespace Marechai.Services
 
             await _context.SaveChangesAsync();
         }
+
+        public bool VerifyUnique(string name) =>
+            !_context.InstructionSets.Any(i => string.Equals(i.Name, name,
+                                                             StringComparison.InvariantCultureIgnoreCase));
     }
 }
