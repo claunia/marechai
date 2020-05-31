9d55f327f0fe95c42f91a8d1428f4d2aef16e976
Natalia Portillo <claunia@claunia.com>
2020-05-31 19:12:50 +0100

    Remove old MVC admin views that are not needed anymore.

--- Marechai/Areas/Admin/Models/BaseViewModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Marechai.Areas.Admin.Models
-{
-    public class BaseViewModel<TKey>
-    {
-        public TKey Id;
-    }
-}
--- Marechai/Areas/Admin/Models/InstructionSetExtensionsByProcessorViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Marechai.Areas.Admin.Models
-{
-    public class InstructionSetExtensionsByProcessorViewModel : BaseViewModel<int>
-    {
-        public string Extension;
-        public string Processor;
-    }
-}
