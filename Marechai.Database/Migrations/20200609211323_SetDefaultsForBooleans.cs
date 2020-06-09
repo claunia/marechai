af07f790fd636703aecb1dedf59b44939810ccd7
Natalia Portillo <claunia@claunia.com>
2020-06-09 22:17:05 +0100

    Set defaults for some other booleans.

---
+++ Marechai.Database/Migrations/20200609211323_SetDefaultsForBooleans.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Marechai.Database.Migrations
+{
+    public partial class SetDefaultsForBooleans : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>("SerialNumberVisible", "OwnedMachines", nullable: false,
+                                               defaultValue: true, oldClrType: typeof(bool), oldType: "bit(1)",
+                                               oldDefaultValue: 1ul);
+
+            migrationBuilder.AlterColumn<bool>("Enabled", "DocumentRoles", nullable: false, defaultValue: true,
+                                               oldClrType: typeof(bool), oldType: "bit(1)", oldDefaultValue: 1ul);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>("SerialNumberVisible", "OwnedMachines", "bit(1)", nullable: false,
+                                               defaultValue: 1ul, oldClrType: typeof(bool), oldDefaultValue: true);
+
+            migrationBuilder.AlterColumn<bool>("Enabled", "DocumentRoles", "bit(1)", nullable: false, defaultValue: 1ul,
+                                               oldClrType: typeof(bool), oldDefaultValue: true);
+        }
+    }
+}
