a6901b8e6bde763e9de63b3c74d3991f70e99020
Natalia Portillo <claunia@claunia.com>
2020-06-10 18:28:00 +0100

    Fix loading Google Analytics.

--- Marechai/wwwroot/js/ga.js
+++ Marechai/wwwroot/js/ga.js
@@ -23,7 +23,12 @@
  // Copyright © 2003-2020 Natalia Portillo
  *******************************************************************************/
 
-window.addEventListener('load', function () {
-    ga('create', 'UA-XXXXXXXX-1', 'auto')
-    ga('send', 'pageview')
-})
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-XXXXX-X']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
