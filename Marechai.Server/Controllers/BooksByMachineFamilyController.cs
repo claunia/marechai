764e058f79365258efa9f9c042430dad1831b56c
Natalia Portillo <claunia@claunia.com>
2025-11-13 17:25:30 +0000

    Add BooksByMachineFamilyController with CRUD operations

---
+++ Marechai.Server/Controllers/BooksByMachineFamilyController.cs
@@ -0,0 +1,105 @@
+/******************************************************************************
+// MARECHAI: Master repository of computing history artifacts information
+// ----------------------------------------------------------------------------
+//
+// Author(s)      : Natalia Portillo <claunia@claunia.com>
+//
+// --[ License ] --------------------------------------------------------------
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as
+//     published by the Free Software Foundation, either version 3 of the
+//     License, or (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// ----------------------------------------------------------------------------
+// Copyright © 2003-2025 Natalia Portillo
+*******************************************************************************/
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Marechai.Data.Dtos;
+using Marechai.Database.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Marechai.Server.Controllers;
+
+[Route("/machine-families/books")]
+[ApiController]
+public class BooksByMachineFamilyController(MarechaiContext context) : ControllerBase
+{
+    [HttpGet("/books/{bookId:long}/machine-families")]
+    [ProducesResponseType(typeof(List<BookByMachineFamilyDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public Task<List<BookByMachineFamilyDto>> GetByBook(long bookId) => context.BooksByMachineFamilies
+       .Where(p => p.BookId == bookId)
+       .Select(p => new BookByMachineFamilyDto
+        {
+            Id              = p.Id,
+            BookId          = p.BookId,
+            MachineFamilyId = p.MachineFamilyId,
+            MachineFamily   = p.MachineFamily.Name
+        })
+       .OrderBy(p => p.MachineFamily)
+       .ToListAsync();
+
+    [HttpDelete("{id:long}")]
+    [Authorize(Roles = "Admin,UberAdmin")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult> DeleteAsync(long id)
+    {
+        string userId = User.FindFirstValue(ClaimTypes.Sid);
+
+        if(userId is null) return Unauthorized();
+
+        BooksByMachineFamily item = await context.BooksByMachineFamilies.FindAsync(id);
+
+        if(item is null) return NotFound();
+
+        context.BooksByMachineFamilies.Remove(item);
+
+        await context.SaveChangesWithUserAsync(userId);
+
+        return Ok();
+    }
+
+    [HttpPost]
+    [Authorize(Roles = "Admin,UberAdmin")]
+    [ProducesResponseType(typeof(int), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [Consumes("application/json")]
+    [Produces("application/json")]
+    public async Task<ActionResult<long>> CreateAsync([FromBody] BookByMachineFamilyDto dto)
+    {
+        string userId = User.FindFirstValue(ClaimTypes.Sid);
+
+        if(userId is null) return Unauthorized();
+
+        var item = new BooksByMachineFamily
+        {
+            MachineFamilyId = dto.MachineFamilyId,
+            BookId          = dto.BookId
+        };
+
+        await context.BooksByMachineFamilies.AddAsync(item);
+        await context.SaveChangesWithUserAsync(userId);
+
+        return item.Id;
+    }
+}
