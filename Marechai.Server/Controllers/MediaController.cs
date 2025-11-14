9847c987b5a8033410597c0dbeddaa4305445195
Natalia Portillo <claunia@claunia.com>
2025-11-14 05:08:14 +0000

    Update copyright year.

--- Marechai.Server/Controllers/MediaController.cs
+++ Marechai.Server/Controllers/MediaController.cs
@@ -20,7 +20,7 @@
 //     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 // ---------------------------------------------------------------------------
-// Copyright © 2003-2025 Natalia Portillo
+// Copyright © 2003-2026 Natalia Portillo
 *******************************************************************************/
 
 using System.Collections.Generic;
