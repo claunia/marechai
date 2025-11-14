14596c5499c3c1c4cd0a591032045a8d9ab27d4f
Natalia Portillo <claunia@claunia.com>
2025-11-14 13:33:27 +0000

    Add Kiota client.

--- Marechai.App/App.xaml.cs
+++ Marechai.App/App.xaml.cs
@@ -3,9 +3,9 @@ using Microsoft.UI.Xaml;
 using Uno.Extensions;
 using Uno.Extensions.Configuration;
 using Uno.Extensions.Hosting;
+using Uno.Extensions.Http;
 using Uno.Extensions.Localization;
 using Uno.Extensions.Navigation;
-using Uno.Resizetizer;
 using Uno.UI;
 
 namespace Marechai.App;
@@ -13,76 +13,94 @@ namespace Marechai.App;
 public partial class App : Application
 {
     /// <summary>
-    /// Initializes the singleton application object. This is the first line of authored code
-    /// executed, and as such is the logical equivalent of main() or WinMain().
+    ///     Initializes the singleton application object. This is the first line of authored code
+    ///     executed, and as such is the logical equivalent of main() or WinMain().
     /// </summary>
     public App()
     {
-        this.InitializeComponent();
+        InitializeComponent();
     }
 
     protected Window? MainWindow { get; private set; }
-    protected IHost? Host { get; private set; }
+    protected IHost?  Host       { get; private set; }
 
-    protected async override void OnLaunched(LaunchActivatedEventArgs args)
+    protected override async void OnLaunched(LaunchActivatedEventArgs args)
     {
-        var builder = this.CreateBuilder(args)
-        // Add navigation support for toolkit controls such as TabBar and NavigationView
-        .UseToolkitNavigation()
-        .Configure(host => host
-#if DEBUG
-        // Switch to Development environment when running in DEBUG
-        .UseEnvironment(Environments.Development)
-#endif
-        .UseLogging(configure: (context, logBuilder) =>
-        {
-            // Configure log levels for different categories of logging
-            logBuilder
-            .SetMinimumLevel(
-                context.HostingEnvironment.IsDevelopment() ? LogLevel.Information : LogLevel.Warning)
+        IApplicationBuilder builder = this.CreateBuilder(args)
 
-            // Default filters for core Uno Platform namespaces
-            .CoreLogLevel(LogLevel.Warning);
-
-            // Uno Platform namespace filter groups
-            // Uncomment individual methods to see more detailed logging
-            //// Generic Xaml events
-            //logBuilder.XamlLogLevel(LogLevel.Debug);
-            //// Layout specific messages
-            //logBuilder.XamlLayoutLogLevel(LogLevel.Debug);
-            //// Storage messages
-            //logBuilder.StorageLogLevel(LogLevel.Debug);
-            //// Binding related messages
-            //logBuilder.XamlBindingLogLevel(LogLevel.Debug);
-            //// Binder memory references tracking
-            //logBuilder.BinderMemoryReferenceLogLevel(LogLevel.Debug);
-            //// DevServer and HotReload related
-            //logBuilder.HotReloadCoreLogLevel(LogLevel.Information);
-            //// Debug JS interop
-            //logBuilder.WebAssemblyLogLevel(LogLevel.Debug);
-        }, enableUnoLogging: true)
-        .UseSerilog(consoleLoggingEnabled: true, fileLoggingEnabled: true)
-        .UseConfiguration(configure: configBuilder =>
-        configBuilder
-        .EmbeddedSource<App>()
-        .Section<AppConfig>()
-        )
-        // Enable localization (see appsettings.json for supported languages)
-        .UseLocalization()
-        .UseHttp((context, services) =>
-        {
+                                           // Add navigation support for toolkit controls such as TabBar and NavigationView
+                                          .UseToolkitNavigation()
+                                          .Configure(host => host
 #if DEBUG
-            // DelegatingHandler will be automatically injected
-            services.AddTransient<DelegatingHandler, DebugHttpHandler>();
+
+                                                             // Switch to Development environment when running in DEBUG
+                                                            .UseEnvironment(Environments.Development)
 #endif
-        })
-        .ConfigureServices((context, services) =>
-        {
-            // TODO: Register your services
-            //services.AddSingleton<IMyService, MyService>();
-        })
-        .UseNavigation(RegisterRoutes)
-        );
+                                                            .UseLogging((context, logBuilder) =>
+                                                                        {
+                                                                            // Configure log levels for different categories of logging
+                                                                            logBuilder
+                                                                               .SetMinimumLevel(context
+                                                                                   .HostingEnvironment
+                                                                                   .IsDevelopment()
+                                                                                    ? LogLevel.Information
+                                                                                    : LogLevel.Warning)
+
+                                                                                // Default filters for core Uno Platform namespaces
+                                                                               .CoreLogLevel(LogLevel.Warning);
+
+                                                                            // Uno Platform namespace filter groups
+                                                                            // Uncomment individual methods to see more detailed logging
+                                                                            //// Generic Xaml events
+                                                                            //logBuilder.XamlLogLevel(LogLevel.Debug);
+                                                                            //// Layout specific messages
+                                                                            //logBuilder.XamlLayoutLogLevel(LogLevel.Debug);
+                                                                            //// Storage messages
+                                                                            //logBuilder.StorageLogLevel(LogLevel.Debug);
+                                                                            //// Binding related messages
+                                                                            //logBuilder.XamlBindingLogLevel(LogLevel.Debug);
+                                                                            //// Binder memory references tracking
+                                                                            //logBuilder.BinderMemoryReferenceLogLevel(LogLevel.Debug);
+                                                                            //// DevServer and HotReload related
+                                                                            //logBuilder.HotReloadCoreLogLevel(LogLevel.Information);
+                                                                            //// Debug JS interop
+                                                                            //logBuilder.WebAssemblyLogLevel(LogLevel.Debug);
+                                                                        },
+                                                                        true)
+                                                            .UseSerilog(true, true)
+                                                            .UseConfiguration(configure: configBuilder =>
+                                                                                  configBuilder.EmbeddedSource<App>()
+                                                                                     .Section<AppConfig>())
+
+                                                             // Enable localization (see appsettings.json for supported languages)
+                                                            .UseLocalization()
+                                                            .UseHttp((context, services) =>
+                                                             {
+#if DEBUG
+
+                                                                 // DelegatingHandler will be automatically injected
+                                                                 services
+                                                                    .AddTransient<DelegatingHandler,
+                                                                         DebugHttpHandler>();
+#endif
+                                                                 services.AddKiotaClientV2<ApiClient>(context,
+                                                                     new EndpointOptions
+                                                                     {
+                                                                         Url = context.Configuration
+                                                                                  .GetSection("ApiClient:Url")
+                                                                                  .Value ??
+
+                                                                               // Fallback to a default URL if not configured
+                                                                               "https://localhost:5023"
+                                                                     });
+                                                             })
+                                                            .ConfigureServices((context, services) =>
+                                                             {
+                                                                 // TODO: Register your services
+                                                                 //services.AddSingleton<IMyService, MyService>();
+                                                             })
+                                                            .UseNavigation(RegisterRoutes));
+
         MainWindow = builder.Window;
 
 #if DEBUG
@@ -95,20 +113,16 @@ public partial class App : Application
 
     private static void RegisterRoutes(IViewRegistry views, IRouteRegistry routes)
     {
-        views.Register(
-            new ViewMap(ViewModel: typeof(ShellViewModel)),
-            new ViewMap<MainPage, MainViewModel>(),
-            new DataViewMap<SecondPage, SecondViewModel, Entity>()
-        );
+        views.Register(new ViewMap(ViewModel: typeof(ShellViewModel)),
+                       new ViewMap<MainPage, MainViewModel>(),
+                       new DataViewMap<SecondPage, SecondViewModel, Entity>());
 
-        routes.Register(
-            new RouteMap("", View: views.FindByViewModel<ShellViewModel>(),
-                Nested:
-                [
-                    new("Main", View: views.FindByViewModel<MainViewModel>(), IsDefault: true),
-                    new("Second", View: views.FindByViewModel<SecondViewModel>()),
-                ]
-            )
-        );
+        routes.Register(new RouteMap("",
+                                     views.FindByViewModel<ShellViewModel>(),
+                                     Nested:
+                                     [
+                                         new RouteMap("Main",   views.FindByViewModel<MainViewModel>(), true),
+                                         new RouteMap("Second", views.FindByViewModel<SecondViewModel>())
+                                     ]));
     }
 }
---
+++ Marechai.App/Services/ServiceCollectionExtensions.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Marechai.App.Services;
+using Microsoft.Kiota.Abstractions.Authentication;
+using Microsoft.Kiota.Http.HttpClientLibrary;
+using Microsoft.Kiota.Serialization.Json;
+using Microsoft.Kiota.Serialization.Multipart;
+
+namespace Uno.Extensions.Http.Kiota;
+
+/// <summary>
+///     Provides extension methods for registering Kiota clients within the <see cref="IServiceCollection" />.
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    ///     Registers a Kiota client with the specified <paramref name="name" /> and endpoint options.
+    /// </summary>
+    /// <typeparam name="TClient">The Kiota client type to register.</typeparam>
+    /// <param name="services">The <see cref="IServiceCollection" /> to register the client with.</param>
+    /// <param name="context">The <see cref="HostBuilderContext" /> providing the hosting context.</param>
+    /// <param name="options">[Optional] The endpoint options for the client (loaded from appsettings if not specified).</param>
+    /// <param name="name">[Optional] The name for locating endpoint information in appsettings.</param>
+    /// <param name="configure">[Optional] A callback for configuring the endpoint.</param>
+    /// <returns>The updated <see cref="IServiceCollection" /> with the registered Kiota client.</returns>
+    public static IServiceCollection AddKiotaClientV2<TClient>(this IServiceCollection services,
+                                                               HostBuilderContext context,
+                                                               EndpointOptions? options = null, string? name = null,
+                                                               Func<IHttpClientBuilder, EndpointOptions,
+                                                                   IHttpClientBuilder>? configure = null)
+        where TClient : class =>
+        services.AddKiotaClientWithEndpointV2<TClient, EndpointOptions>(context, options, name, configure);
+
+    /// <summary>
+    ///     Registers a Kiota client with the specified <paramref name="name" /> and supports additional endpoint options.
+    /// </summary>
+    /// <typeparam name="TClient">The Kiota client type to register.</typeparam>
+    /// <typeparam name="TEndpoint">The type of endpoint to register.</typeparam>
+    /// <param name="services">The <see cref="IServiceCollection" /> to register the client with.</param>
+    /// <param name="context">The <see cref="HostBuilderContext" /> providing the hosting context.</param>
+    /// <param name="options">[Optional] The endpoint options for the client (loaded from appsettings if not specified).</param>
+    /// <param name="name">[Optional] The name for locating endpoint information in appsettings.</param>
+    /// <param name="configure">[Optional] A callback for configuring the endpoint.</param>
+    /// <returns>The updated <see cref="IServiceCollection" /> with the registered Kiota client.</returns>
+    public static IServiceCollection AddKiotaClientWithEndpointV2<TClient, TEndpoint>(
+        this IServiceCollection services, HostBuilderContext context, TEndpoint? options = null, string? name = null,
+        Func<IHttpClientBuilder, TEndpoint?, IHttpClientBuilder>? configure = null)
+        where TClient : class where TEndpoint : EndpointOptions, new()
+    {
+        services.AddKiotaHandlers();
+        string clientName = name ?? typeof(TClient).FullName ?? "DefaultClient";
+
+        return services.AddClientWithEndpoint<TClient, TEndpoint>(context,
+                                                                  options,
+                                                                  clientName,
+                                                                  (s, c) => s.AddHttpClient<TClient>(clientName)
+                                                                             .AddTypedClient((httpClient, sp) =>
+                                                                              {
+                                                                                  var authProvider =
+                                                                                      new
+                                                                                          AnonymousAuthenticationProvider();
+
+                                                                                  var parseNodeFactory =
+                                                                                      new JsonParseNodeFactory();
+
+                                                                                  var serializationWriterFactory =
+                                                                                      new
+                                                                                          CompositeSerializationWriterFactory();
+
+                                                                                  serializationWriterFactory
+                                                                                     .AddFactory(new
+                                                                                          JsonSerializationWriterFactory());
+
+                                                                                  serializationWriterFactory
+                                                                                     .AddFactory(new
+                                                                                          MultipartSerializationWriterFactory());
+
+                                                                                  var requestAdapter =
+                                                                                      new
+                                                                                          HttpClientRequestAdapter(authProvider,
+                                                                                              parseNodeFactory,
+                                                                                              serializationWriterFactory,
+                                                                                              httpClient);
+
+                                                                                  return (TClient)
+                                                                                      Activator
+                                                                                         .CreateInstance(typeof(
+                                                                                                  TClient),
+                                                                                              requestAdapter)!;
+                                                                              })
+                                                                             .AttachKiotaHandlers(),
+                                                                  configure);
+    }
+
+    /// <summary>
+    ///     Dynamically adds Kiota handlers to the service collection.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection" /> to register the handlers with.</param>
+    /// <returns>The updated <see cref="IServiceCollection" /> with the registered Kiota handlers.</returns>
+    private static IServiceCollection AddKiotaHandlers(this IServiceCollection services)
+    {
+        IList<Type> kiotaHandlers = KiotaClientFactory.GetDefaultHandlerTypes();
+        foreach(Type handler in kiotaHandlers) services.AddTransient(handler);
+
+        return services;
+    }
+
+    /// <summary>
+    ///     Attaches Kiota handlers to the <see cref="IHttpClientBuilder" />.
+    /// </summary>
+    /// <param name="builder">The <see cref="IHttpClientBuilder" /> to attach the handlers to.</param>
+    /// <returns>The updated <see cref="IHttpClientBuilder" /> with the attached Kiota handlers.</returns>
+    private static IHttpClientBuilder AttachKiotaHandlers(this IHttpClientBuilder builder)
+    {
+        IList<Type> kiotaHandlers = KiotaClientFactory.GetDefaultHandlerTypes();
+
+        foreach(Type handler in kiotaHandlers)
+            builder.AddHttpMessageHandler(sp => (DelegatingHandler)sp.GetRequiredService(handler));
+
+        return builder;
+    }
+}
