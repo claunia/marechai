956715337864918c5b661d2207c5ff6b36ede79a
Natalia Portillo <claunia@claunia.com>
2025-11-16 18:22:25 +0000

    Add missing navigation parameters.

---
+++ Marechai.App/Presentation/Models/SoundSynthDetailNavigationParameter.cs
@@ -0,0 +1,9 @@
+#nullable enable
+
+namespace Marechai.App.Presentation.Models;
+
+public class SoundSynthDetailNavigationParameter
+{
+    public int     SoundSynthId     { get; set; }
+    public object? NavigationSource { get; set; }
+}
