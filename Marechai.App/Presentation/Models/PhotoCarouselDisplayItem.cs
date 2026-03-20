fc80accf8a51e68908f701f4a69e8d72ef9adaf1
Natalia Portillo <claunia@claunia.com>
2026-03-20 19:32:00 +0000

    Fixed photos not loading in photo carousel in machine details view.

--- Marechai.App/Presentation/Models/PhotoCarouselDisplayItem.cs
+++ Marechai.App/Presentation/Models/PhotoCarouselDisplayItem.cs
@@ -6,10 +6,12 @@ namespace Marechai.App.Presentation.Models;
 /// <summary>
 ///     Display item for photo carousel
 /// </summary>
-public class PhotoCarouselDisplayItem
+public partial class PhotoCarouselDisplayItem : ObservableObject
 {
     // Thumbnail constraints
-    public const int          ThumbnailMaxSize = 256;
-    public       Guid         PhotoId              { get; set; }
-    public       ImageSource? ThumbnailImageSource { get; set; }
+    public const int ThumbnailMaxSize = 256;
+    public       Guid PhotoId { get; set; }
+
+    [ObservableProperty]
+    private ImageSource? _thumbnailImageSource;
 }
