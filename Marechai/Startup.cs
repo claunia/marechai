f478d09d65af9f258a54e0495d4a1ad1d58de63b
Natalia Portillo <claunia@claunia.com>
2026-04-27 10:53:07 +0100

    [Blazor] Add French localization

--- Marechai/Startup.cs
+++ Marechai/Startup.cs
@@ -50,7 +50,7 @@ public class Startup(IConfiguration configuration)
 {
     readonly CultureInfo[] _supportedCultures =
     [
-        new("en-US"), new("es")
+        new("en-US"), new("es"), new("fr")
     ];
 
     public IConfiguration Configuration { get; } = configuration;
