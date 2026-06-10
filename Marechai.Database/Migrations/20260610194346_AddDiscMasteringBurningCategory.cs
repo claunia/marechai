34d73dc6b1672d79b690b8c23d49cce09ea3e76b
Natalia Portillo <claunia@claunia.com>
2026-06-10 20:48:53 +0100

    Add migration to seed "Disc Mastering & Burning" SoftwareGenreType.Category
    This migration inserts a new entry for the "Disc Mastering & Burning" category into the SoftwareGenres table if it does not already exist. The entry has a Type of 4 and includes timestamps for CreatedOn and UpdatedOn. The Down method ensures that the entry can be removed if necessary.

---
+++ Marechai.Database/Migrations/20260610194346_AddDiscMasteringBurningCategory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Marechai.Database.Migrations;
+
+/// <summary>
+///     Seeds one additional <c>SoftwareGenreType.Category</c> (= 4) entry:
+///     "Disc Mastering &amp; Burning".  Idempotent via <c>WHERE NOT EXISTS</c>.
+/// </summary>
+public partial class AddDiscMasteringBurningCategory : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql(@"
+INSERT INTO `SoftwareGenres` (`Name`, `Type`, `CreatedOn`, `UpdatedOn`) SELECT 'Disc Mastering & Burning', 4, UTC_TIMESTAMP(6), UTC_TIMESTAMP(6) WHERE NOT EXISTS (SELECT 1 FROM `SoftwareGenres` WHERE `Name`='Disc Mastering & Burning' AND `Type`=4);
+");
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.Sql("DELETE FROM `SoftwareGenres` WHERE `Type` = 4 AND `Name` = 'Disc Mastering & Burning';");
+    }
+}
